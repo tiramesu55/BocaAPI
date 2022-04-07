8fea1e2ed940aec0c9ccad44f636946c1d33044b
Alec Goldis <agoldis@trimontrea.com>
2022-04-07 19:55:46 -0400

    loop

--- Boca.API/Repository/BocaRepository.cs
+++ Boca.API/Repository/BocaRepository.cs
@@ -14,7 +14,7 @@ namespace BocaAPI.Repository
         {
             db = new SqlConnection(connectionString);
         }
-        
+
 
         public async Task<List<PoliceCode>> GetPoliceCodes() => (await db.QueryAsync<PoliceCode>("SELECT * FROM dbo.police_codes")).ToList();
 
@@ -24,79 +24,57 @@ namespace BocaAPI.Repository
 
         public async Task<IEnumerable<RawExportData>> UploadToDatabase(List<VCSExport> records)
         {
-            // truncate receiving table
-            await db.ExecuteAsync("truncate table temp_police_master; ") ;
-             
-            //insert into temp
-            await db.ExecuteAsync(
-                @"INSERT into temp_police_master
-                  ([payid],[wcpid],[Wcabr],[reasoncode],[reason],[rosdate],[strdate],[enddate],[shftab],[removed],[rectype],[payduration],[comment])  
-                  VALUES(@PAYID,
-                           @WCPID,
-                           @WCABR,  
-                           @ReasonCode,
-                           @Reason,
-                           @ROSDT,
-                           @STRDT,
-                           @ENDDT,
-                           @SHFTAB,
-                           @REMOVED,
-                           @RECTYP,
-                           @PAYDURAT,
-                           @Comment)", records);
+            await db.ExecuteAsync("truncate table NewlyInserted; ");
 
-            //this section should go first inside next queryAsync call if we use temporary table method
-            //For now we are using a real table and clearing it when we run export
-            //
-            //await db.ExecuteAsync(
-            // @"
-            //        CREATE TABLE #NewlyInserted(
-	           //         PayId  int,
-	           //         WcpId [nvarchar](8),
-	           //         ROSDate [smalldatetime],
-	           //         PayDuration [numeric](18, 3),
-	           //         [Comment] [nvarchar](1028)
-            //         )"
-            // );
+            foreach (var rec in records)
+            {
 
-            var rtn = await db.QueryAsync<RawExportData>(
-            @"
---  we are using real table in this version
---            CREATE TABLE #NewlyInserted(
---	                PayId  int,
---                    WcpId[nvarchar](8),
---                    ROSDate[smalldatetime],
---                    PayDuration[numeric](18, 3),
---                    [Comment][nvarchar](1028)
---                    );
-            MERGE INTO police_master t 
-                USING temp_police_master s
-                   ON t.payid = s.payid
-                   AND s.[WcpId] = t.[wcpid]
-                   AND t.[Wcabr] = s.[Wcabr]
-                   AND t.[rosdate] = s.[ROSDate]
-                   AND t.[strdate] = s.[STRDate]
-                   AND t.[enddate] = s.[ENDDate]
-                   AND t.[shftab] = s.[SHFTAB]
-                WHEN NOT MATCHED THEN 
-                    INSERT ([payid],[wcpid],[Wcabr],[reasoncode],[reason],[rosdate],[strdate],[enddate],[shftab],[removed],[rectype],[payduration],[comment])
-                    VALUES ( s.[payid],
-                           s.[wcpid],
-                           s.[Wcabr],
-                           s.[reasoncode],
-                           s.[reason],
-                           s.[rosdate],
-                           s.[strdate],
-                           s.[enddate],
-                           s.[shftab],
-                           s.[removed],
-                           s.[rectype],
-                           s.[payduration],
-                           s.[comment] ) 
-                           OUTPUT inserted.payid, inserted.wcpid,inserted.rosdate, inserted.payduration, inserted.comment INTO NewlyInserted ;
-                  select * from NewlyInserted;   --If we decide to use temp table we need to  change NewlyInserted to #NewlyInserted everywhere
-                "
-                );
+                await db.ExecuteAsync(
+                @"
+                MERGE INTO police_master t 
+                    USING ( VALUES 
+                        ( @PAYID,
+                          @WCPID,
+                          @WCABR,
+                          @ReasonCode,
+                          @Reason,
+                          @ROSDT,
+                          @STRDT,
+                          @ENDDT,
+                          @SHFTAB,
+                          @REMOVED,
+                          @RECTYP,
+                          @PAYDURAT,
+                          @Comment ) ) s([PayId], [WcpId],[WCABR], [ReasonCode], [Reason], [ROSDate],
+                          [STRDate], [ENDDate], [SHFTAB], [Removed], [RecType], [PayDuration],
+                          [Comment])
+                    ON t.payid = s.payid
+                       AND s.[WcpId] = t.[wcpid]
+                       AND t.[Wcabr] = s.[Wcabr]
+                       AND t.[rosdate] = s.[ROSDate]
+                       AND t.[strdate] = s.[STRDate]
+                       AND t.[enddate] = s.[ENDDate]
+                       AND t.[shftab] = s.[SHFTAB]
+                    WHEN NOT MATCHED THEN 
+                        INSERT ([payid],[wcpid],[Wcabr],[reasoncode],[reason],[rosdate],[strdate],[enddate],[shftab],[removed],[rectype],[payduration],[comment])
+                        VALUES ( s.[payid],
+                               s.[wcpid],
+                               s.[Wcabr],
+                               s.[reasoncode],
+                               s.[reason],
+                               s.[rosdate],
+                               s.[strdate],
+                               s.[enddate],
+                               s.[shftab],
+                               s.[removed],
+                               s.[rectype],
+                               s.[payduration],
+                               s.[comment] ) 
+                               OUTPUT inserted.payid, inserted.wcpid,inserted.rosdate, inserted.payduration, inserted.comment INTO NewlyInserted ;
+                    ", rec
+                        );
+            }
+            var rtn = await db.QueryAsync<RawExportData>(" select * from NewlyInserted"); // --If we decide to use temp table we need to change NewlyInserted to #NewlyInserted everywhere
             return rtn;
 
         }
@@ -106,7 +84,7 @@ namespace BocaAPI.Repository
             var rtn = await db.QueryAsync<RawExportData>(
                 @"SELECT * FROM NewlyInserted;"
                );
-            await db.ExecuteAsync("truncate table NewlyInserted; ");
+          //  await db.ExecuteAsync("truncate table NewlyInserted; ");
             return rtn;
         }
 
